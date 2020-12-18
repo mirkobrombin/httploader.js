60b857c6641077c9bbe4e64c9ca1cf572fc7ce70
Mirko Brombin <send@mirko.pm>
2020-12-18 08:18:37 +0100

    Update httploader.js

--- httploader.js
+++ httploader.js
@@ -1,3 +1,21 @@
+/* https://git.mirko.pm/brombinmirko/httploader-js 
+ * 
+ * Copyright (c) 2020 Mirko Brombin <send@mirko.pm>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 function load(dom, url, source_dom=false)
 {
     let body = document.getElementsByTagName("body")[0];
@@ -9,20 +27,11 @@ function load(dom, url, source_dom=false)
             content = xml_http.responseText;
             if(source_dom)
             {
-                try
-                {
-                    document.getElementById("content_process").remove();
-                }
-                catch
-                {
-                    console.debug("No content process found.")
-                }
                 content_process = document.createElement("div"),
                 content_process.innerHTML = content,
                 content_process.id = "content_process",
                 content_process.style.display = "none",
-                body.appendChild(content_process),
-                content = document.querySelector("#content_process " + source_dom).outerHTML
+                content = content_process.querySelector(source_dom).outerHTML
             }
             dom.innerHTML = content;
         }
